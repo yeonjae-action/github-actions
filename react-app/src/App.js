d192f76f9dbac98fa910c2172559a5670270cf74
yeonjaedev <yjae47@gmail.com>
2023-12-12 15:14:58 +0900

    [UPDATE] react-app 수정2

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test 2
+          Learn Github Action 2
         </a>
       </header>
     </div>
