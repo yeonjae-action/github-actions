ca6dbc55b894c46b8d7e203b2c40259b2ba57a85
yeonjaedev <yjae47@gmail.com>
2023-12-18 17:58:50 +0900

    [UPDATE] App.js 수정

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Github Action CI/CD test 1
+          Learn Github Action CI/CD test 2
         </a>
       </header>
     </div>
