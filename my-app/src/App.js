bbd0428b2cdcd02220539346f52e667e14d509bc
yeonjaedev <yjae47@gmail.com>
2023-12-19 14:43:31 +0900

    [UPDATE] 재수정 - synchronize 이벤트 발생

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd1 aws1
+          Learn GithubAction cicd1 aws1 test
         </a>
       </header>
     </div>
