dc5201a86c8103cf10370e7636103e3716118ecb
yeonjaedev <yjae47@gmail.com>
2023-12-19 16:38:47 +0900

    [UPDATE]

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction cicd1 aws1 test 2
+          Learn GithubAction cicd1 aws1 test 2 3
         </a>
       </header>
     </div>
